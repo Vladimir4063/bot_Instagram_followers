e126944389dde43b72817189cfba08b954c249db
Vladimir Gutierrez <vladimirgutierrez4063@gmail.com>
2021-02-10 00:27:23 -0300

    Subiendo bot de Instagram_followers

---
+++ main.py
@@ -0,0 +1,10 @@
+from instabot import Bot
+
+"""Colocando este codigo: mi_bot = Bot(filter_users=False) envia solicitudes a cuentas privadas
+"""
+
+mi_bot = Bot()
+mi_bot.login(username='vladimirgutierrez7', password='contravlady')
+mi_bot.follow_followers('padavidnesher')
+
+
